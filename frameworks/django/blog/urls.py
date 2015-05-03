ff0f849f809f352f27e1cac336e42c0f58cf76c5
Tony Lattke <tonylattke@gmail.com>
2015-05-03 22:16:48 +0200

    Index para posts

--- frameworks/django/blog/urls.py
+++ frameworks/django/blog/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+	url(r'^$', 'posts.views.index', name='home'),
+	
+	url(r'^posts/', include('posts.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
 )
--- frameworks/django/posts/views.py
+++ frameworks/django/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+	all_posts = Post.objects.all()
+	context = {'post_list': all_posts}
+	return render(request, 'index.html', context)
