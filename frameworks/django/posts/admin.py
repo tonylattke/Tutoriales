76c0511256a178af47a7ea197c346753ab7bcfd2
Tony Lattke <tonylattke@gmail.com>
2015-05-04 21:37:44 +0200

    Admin!

--- frameworks/django/posts/admin.py
+++ frameworks/django/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+	list_display= ('id', 'nombre', 'pub_fecha', 'autor')
+
+admin.site.register(Post, PostAdmin)
