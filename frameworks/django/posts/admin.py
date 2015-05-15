b69d932f59e3dd70d511dbf1f3139094988a3429
Tony Lattke <tonylattke@gmail.com>
2015-05-15 11:50:41 +0200

    Nuevas congiguraciones en Admin

--- frameworks/django/posts/admin.py
+++ frameworks/django/posts/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django import forms
 
 from .models import Post
 
+''' ******************************* Ayudas ******************************* '''
+#Formato Form
+class Text_Form(forms.ModelForm):
+	text = forms.CharField( widget=forms.Textarea )
+
+''' ********************************* Post ******************************** '''
 class PostAdmin(admin.ModelAdmin):
-	list_display= ('id', 'nombre', 'pub_fecha', 'autor')
+	list_display= ('id', 'nombre', 'pub_fecha','publicado_recientemente', 'autor')
+	form = Text_Form
+	search_fields = ['nombre','contenido']
+	fieldsets 	= [
+		('Nombre Post'	,	{'fields': ['nombre']}),
+		('Fecha'		,	{'fields': ['pub_fecha']}),
+		('Autor'		,	{'fields': ['autor']}),
+		('Contenido'	,	{'fields': ['contenido']}),
+	]
 
 admin.site.register(Post, PostAdmin)
--- frameworks/django/posts/models.py
+++ frameworks/django/posts/models.py
@@ -1,11 +1,19 @@
-from django.db import models
+from django.db    import models
+from django.utils import timezone
 
+from datetime import timedelta
 
 class Post(models.Model):
 	nombre = models.CharField(max_length=200)
 	autor = models.CharField(max_length=200)
 	contenido = models.TextField()
 	pub_fecha = models.DateTimeField('fecha')
+
+	def publicado_recientemente(self):
+		return self.pub_fecha >= (timezone.now() - timedelta(days=1))
+	publicado_recientemente.admin_order_field = 'pub_fecha'
+	publicado_recientemente.boolean = True
+	publicado_recientemente.short_description = 'Publicado recientemente?'
 
 class Comentario(models.Model):
 	autor = models.CharField(max_length=200, default='Tony')
